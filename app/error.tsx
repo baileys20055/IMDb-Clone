72124737f4a8d2549c1573f8ef456200d1d7fc4e
Shae Bailey <81445894+baileys20055@users.noreply.github.com>
2023-06-17 17:55:47 -0400

    Added error page to handle errors

---
+++ app/error.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { error } from 'console';
+import { useEffect } from 'react';
+
+import React from 'react'
+
+type Props = { error: Error, reset: any }
+
+export default function Error({ error, reset }: Props) {
+    useEffect(() => {
+        console.log(error);
+    }, [error]);
+
+  return (
+    <div className='text-center mt-10'>
+        <h1>Something went wrong...</h1>
+        <button className="hover:text-amber-600" onClick={() => reset()}>Try Again</button>
+    </div>
+  )
+}
