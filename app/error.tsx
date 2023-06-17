2dce7edb64ea793f71e78289b443ccf01ba78d7c
Shae Bailey <81445894+baileys20055@users.noreply.github.com>
2023-06-17 18:56:37 -0400

    formatted project with prettier

--- app/error.tsx
+++ app/error.tsx
@@ -1,18 +1,20 @@
-"use client";
+'use client';
 import { useEffect } from 'react';
-import React from 'react'
+import React from 'react';
 
-type Props = { error: Error, reset: any }
+type Props = { error: Error; reset: any };
 
 export default function Error({ error, reset }: Props) {
-    useEffect(() => {
-        console.log(error);
-    }, [error]);
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
 
   return (
-    <div className='text-center mt-10'>
-        <h1>Something went wrong...</h1>
-        <button className="hover:text-amber-600" onClick={() => reset()}>Try Again</button>
+    <div className="text-center mt-10">
+      <h1>Something went wrong...</h1>
+      <button className="hover:text-amber-600" onClick={() => reset()}>
+        Try Again
+      </button>
     </div>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-export const dynamic = "force-dynamic"
-import MovieResult from "@/components/MovieResult";
+export const dynamic = 'force-dynamic';
+import MovieResult from '@/components/MovieResult';
 
 const API_KEY = process.env.API_KEY;
 
-type Props = {searchParams: any};
+type Props = { searchParams: any };
 
 interface MovieInfo {
   adult: boolean;
@@ -23,24 +23,25 @@ interface MovieInfo {
   origin_country?: number[];
 }
 
-
 export default async function Home({ searchParams }: Props) {
-  const genre = searchParams.genre || "fetchTrending";
-  const res = await fetch(`https://api.themoviedb.org/3/${
-    genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
-  }?api_key=${API_KEY}`, { next: { revalidate: 10000 }});
+  const genre = searchParams.genre || 'fetchTrending';
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/all/week'
+    }?api_key=${API_KEY}`,
+    { next: { revalidate: 10000 } }
+  );
 
   if (!res.ok) {
-    throw new Error("Failed to fetch movie data");
+    throw new Error('Failed to fetch movie data');
   }
 
   const data: any = await res.json();
   const results = data.results;
 
-
   return (
-  <div>
-    <MovieResult results={results} />
-  </div>
+    <div>
+      <MovieResult results={results} />
+    </div>
   );
 }
